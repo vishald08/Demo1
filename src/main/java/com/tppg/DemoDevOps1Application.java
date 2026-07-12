7591d64a08986aefe1ff54565bf6fbbaf03cd246
VishalDeshmukh <vishaldeshmukh963@gmail.com>
2026-07-12 19:39:08 +0530

    disable datasource configure for docker test

--- src/main/java/com/tppg/DemoDevOps1Application.java
+++ src/main/java/com/tppg/DemoDevOps1Application.java
@@ -2,8 +2,9 @@ package com.tppg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DemoDevOps1Application {
 
 	public static void main(String[] args) {
